fbe2cd38fa4b1bafb5d310f745034e942e778ce8
Tushar Maharanagit <tusharhero@sdf.org>
2023-01-30 20:19:54 +0530

    Add description for every post.

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from feedgen.feed import FeedGenerator
 def getvidId(link):
     return link.split("v=")[1].split("&")[0]
 
+def makeDescription(link="", authors="unknown", description="description unavailable"):
+    Desc = str(f" <a href='{link}'>link</a>\
+            </br><b> {authors} </b> </br>\
+            </br>{description}</br>\
+            <i>\
+            </br> fetched using Invidous and Piped. </br> \
+            ytrss - a hack by <a href='mailto:tusharhero@sdf.org'>tusharhero</a>\
+            </i>\
+            ")
+    return Desc
+
 def createfeed(feed):
     fd = FeedGenerator()
     fd.logo(feed['feed']['icon'])
@@ -14,11 +25,7 @@ def createfeed(feed):
         fe = fd.add_entry()
         fe.title(item['title'])
         fe.link(href=item['link'])
-        fe
         vidId = getvidId(item['link'])
+        fe.description(makeDescription(link=item['link'],authors=item['authors'][0]['name']))
         fe.enclosure(f"https://invidious.kavin.rocks/latest_version?id={vidId}&itag=140&listen=1",0,'audio/m4a')
     return fd
-
-
-feed = fp.parse("https://pipedapi.kavin.rocks/feed/unauthenticated/rss?channels=UCPxMZIFE856tbTfdkdjzTSQ")
-createfeed(feed).rss_file("ranveer.rss")
