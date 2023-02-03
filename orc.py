29d3fc95b90073d231d178db79a447ffed4980fc
Tushar Maharanagit <tusharhero@sdf.org>
2023-02-03 15:25:14 +0530

    code formatting improved

--- orc.py
+++ orc.py
@@ -1,14 +1,16 @@
 import get, gen
 import tomlkit
 
+
 def loadconfig(config="./config.toml"):
     f = open(config).read()
     configuration = tomlkit.parse(f)
     return configuration
 
+
 def main():
     config = loadconfig()
-    for channel in config['channels']:
-        videodata = get.getvideodata(channel['channelid'])
+    for channel in config["channels"]:
+        videodata = get.getvideodata(channel["channelid"])
         feed = gen.createfeed(videodata)
         gen.savefeed(feed)
